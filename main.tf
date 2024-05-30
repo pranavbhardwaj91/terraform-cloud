39aa504490bb38d346a4da0669dec27256f4e698
pranavbhardwaj91 <pranavbhardwaj91@gmail.com>
2024-05-30 13:26:21 +0530

    add ssh

--- main.tf
+++ main.tf
@@ -37,6 +37,29 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
 resource "azurerm_linux_virtual_machine" "example" {
   name                  = "example-machine"
   location              = azurerm_resource_group.example.location
